58f20de5c79bc80d55a5bcb683515e9fc1c5924d
spence <26928903+sgconline@users.noreply.github.com>
2025-03-17 11:40:28 +0000

    Add contact functionality v0.1

--- contacts/app/contacts.py
+++ contacts/app/contacts.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 def home():
   return render_template(
          "index.html",
-          contacts=db,
+          contacts=db
+          
     )
 
 @app.route("/contact/<int:index>")
@@ -29,3 +30,34 @@ def remove_contact(index):
         return render_template("remove_contact.html", contact=db[index])
   except IndexError:
     abort(404)
+
+@app.route("/add_contact", methods=['POST','GET'])
+def add_contact():
+   if request.method == "POST":
+      next_primary_key = db[len(db)-1]["pk"] + 1
+      contact = {"pk": next_primary_key, 
+                 "model": "addressbook.Contact",
+                 "fields": 
+                  {"city": request.form['city'],
+                    "first_name": request.form['firstname'],
+                    "last_name": request.form['surname'],
+                    "zip": request.form['zip'],
+                    "title": request.form['title'],
+                    "address1": request.form['address1'],
+                    "address2": request.form['address2'],
+                    "address3": request.form['address3'],
+                    "mi": "", 
+                    "fax": "",
+                    "email": request.form['email'],
+                    "phone": request.form['telephone'],
+                    "state": request.form['state'],
+                    "country": request.form['country'],
+                    "notes": "", 
+                    "grad_class": ""
+                  }
+                }
+      db.append(contact)
+      save_db()
+      return redirect(url_for("contact_view", index=len(db)-1))
+   else:
+      return render_template("add_contact.html")
